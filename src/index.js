8e7e874d2c07e60c1dbee1a1e854d2ec2e31be56
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-23 23:29:44 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+
+
+
+
 ReactDOM.render(
   meInReact,
   document.getElementById('global')
 );
 
 export default meInReact
-*/
